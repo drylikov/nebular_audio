5a138ddd37c6dd0d8b4ce1486e62c8d30e2ae110
drylikov <denis.rylikov@protonmail.com>
2025-11-11 12:46:03 +0300

    nebular_audio
    Audio player system for Swift .

---
+++ NebularAudio/NebularAudio.h
@@ -0,0 +1,18 @@
+
+//
+//  NebularAudio.h
+//  NebularAudio
+//
+//  Created by Linus Schwalbe on 2025-10-08.
+//  Copyright © 2025 drylikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NebularAudio.
+FOUNDATION_EXPORT double NebularAudioVersionNumber;
+
+//! Project version string for NebularAudio.
+FOUNDATION_EXPORT const unsigned char NebularAudioVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NebularAudio/PublicHeader.h>
